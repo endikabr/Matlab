79ec1a5848be82c301b070932b276344326dd4cc
endikabr <endikabr@gmail.com>
2024-11-12 18:46:15 +0100

    Create Jacobi.m

---
+++ Jacobi.m
@@ -0,0 +1,24 @@
+function [xn, k] = jacobi(A,b,tol,x0)
+
+n = length(A);
+xn = [];
+error = 1;
+k = 0;
+
+
+while error > tol && k < 1000
+    xn(1,1) = (1/A(1,1))*(b(1)-A(1,2:n)*x0(2:n));
+    
+    for i = 2:n
+       
+        sum1 = A(i,1:i-1)*x0(1:i-1,1);
+        sum2 = A(i,i+1:n)*x0(i+1:n,1);
+        xn(i,1) = (1/A(i,i))*(b(i)-sum1-sum2);
+
+    end
+
+    error = max(abs(xn-x0))/max(abs(xn))
+    x0 = xn;
+    k = k + 1;
+
+end
